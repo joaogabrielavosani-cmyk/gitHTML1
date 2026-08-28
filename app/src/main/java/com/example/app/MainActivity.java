661779055192260d9c1d4b3cce68c228e826451c
LucasIudyRech <lucasiudyrech20@gmail.com>
2026-08-28 12:08:38 -0300

    Aula 2, programa simples de sorteio de n°

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +11,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     int c=0;
     @Override
@@ -17,19 +20,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button b=  findViewById(R.id.button);
+        /*Button b=  findViewById(R.id.button);
 
         TextView tv= findViewById(R.id.textView2);
 
         b.setOnClickListener(view -> {
             c++;
             tv.setText(Integer.toString(c));
-        });
+        }); */
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Associar componetes da IU
+        EditText edMin, edMax;
+        edMin = findViewById(R.id.edMIn);
+        edMax = findViewById(R.id.edMax);
+
+        TextView textView = findViewById(R.id.textView2);
+        Button s= findViewById(R.id.button);
+
+
+        s.setOnClickListener(view -> {
+            Random random = new Random();
+            String smin= edMin.getText().toString();
+            String smax= edMax.getText().toString();
+            if (smin.trim().equals("")){
+                edMin.setError("Informar valor");
+                edMin.requestFocus();
+                return;
+            }
+
+            if (smax.trim().equals("")){
+                edMax.setError("Informar valor");
+                edMax.requestFocus();
+                return;
+            }
+
+
+            int min = Integer.parseInt(edMin.getText().toString());
+            int max = Integer.parseInt(edMax.getText().toString());
+            if (min>=max){
+                edMax.setError("Mínimo deve ser menor que o máximo");
+                return;
+            }
+
+            int r= (random.nextInt(max-min)+min);
+
+            textView.setText(Integer.toString(r)); //setar valor
+        });
+
     }
 }
